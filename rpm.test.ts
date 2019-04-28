382046b55137864af3ad05ade154475dd949c061
Alex Olson <alex.olson@gis-ag.com>
2019-04-28 19:10:47 +0200

    basic io tests done. Now to add some spy tests.

--- rpm.test.ts
+++ rpm.test.ts
@@ -1,6 +1,21 @@
 import rpm from './rpm';
 
+const genRandInt: () => number = () => Math.floor(Math.random() * 1000001) - 500000;
+
 describe('rpm', () => {
+  let leftOperand: number;
+  let rightOperand: number;
+
+  beforeAll(() => {
+    [leftOperand, rightOperand] = [1, 2].map(() => genRandInt());
+
+    console.log('Randomly generated operands:');
+    console.log({
+      leftOperand,
+      rightOperand,
+    });
+  });
+
   it('returns 0 if left operand provided is zero', () => {
     expect(rpm(0, -1)).toBe(0);
   });
@@ -55,5 +70,9 @@ describe('rpm', () => {
 
   it('correctly multplies -100000 and 100000', () => {
     expect(rpm(-100000, 100000)).toBe(-10000000000);
+  });
+
+  it('correctly multiplies randomly generated left and right operands', () => {
+    expect(rpm(leftOperand, rightOperand)).toBe(leftOperand * rightOperand);
   });
 });
