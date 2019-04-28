6bd26adadc63e1b92825b9b8420369c8fd831d7a
Alex Olson <alex.olson@gis-ag.com>
2019-04-28 04:53:42 +0200

    positive numbers working, negative numbers still need work.

--- rpm.test.ts
+++ rpm.test.ts
@@ -1,7 +1,47 @@
 import rpm from './rpm';
 
 describe('rpm', () => {
+  it('returns 0 if left operand provided is zero', () => {
+    expect(rpm(0, -1)).toBe(0);
+  });
+
+  it('returns 0 if right operand provided is zero', () => {
+    expect(rpm(-1, 0)).toBe(0);
+  });
+
   it('returns 0 if both operands provided are zero', () => {
     expect(rpm(0, 0)).toBe(0);
   });
+
+  it('correctly multplies 5 and 2', () => {
+    expect(rpm(5, 2)).toBe(10);
+  });
+
+  it('correctly multplies 2 and 5', () => {
+    expect(rpm(2, 5)).toBe(10);
+  });
+
+  it('correctly multplies -5 and 2', () => {
+    expect(rpm(-5, 2)).toBe(-10);
+  });
+
+  it('correctly multplies 5 and -2', () => {
+    expect(rpm(5, -2)).toBe(-10);
+  });
+
+  it('correctly multplies -5 and -2', () => {
+    expect(rpm(-5, -2)).toBe(10);
+  });
+
+  it('correctly multplies -2 and 5', () => {
+    expect(rpm(-2, 5)).toBe(-10);
+  });
+
+  it('correctly multplies 2 and -5', () => {
+    expect(rpm(2, -5)).toBe(-10);
+  });
+
+  it('correctly multplies -2 and -5', () => {
+    expect(rpm(-2, -5)).toBe(10);
+  })
 });
--- rpm.ts
+++ rpm.ts
@@ -1,3 +1,40 @@
+type MultRow = [number, number];
+type MultTable = MultRow[];
+
 export default function rpm(x: number, y: number): number {
-  return 0;
+  let multTable: MultTable = [];
+
+  multTable.push([x, y]);
+
+  return rpmWorker(multTable);
 }
+
+
+const rpmWorker: (multTable: MultTable) => number = (multTable) => {
+  const lastRow: MultRow = multTable[multTable.length - 1];
+
+  console.log(multTable);
+
+  if ([-1, 1, 0].includes(lastRow[0])) {
+    return multTable
+      .map((row: MultRow): MultRow => {
+        let [left, right]: [number, number] = row;
+        right = (() => {
+          if (left % 2 === 0) {
+            return 0;
+          }
+
+          return right;
+        })();
+
+        return [left, right];
+      })
+      .reduce((acc, row) => {
+        return acc + row[1];
+      }, 0);
+  }
+
+  multTable.push([Math.floor(lastRow[0] / 2), lastRow[1] * 2]);
+
+  return rpmWorker(multTable);
+};
