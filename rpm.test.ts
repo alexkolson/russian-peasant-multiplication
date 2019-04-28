c19374f4fa554cbda958eb3f9dba49b484b6995c
Alex Olson <alex.olson@gis-ag.com>
2019-04-29 01:18:45 +0200

    fix spelling.

--- rpm.test.ts
+++ rpm.test.ts
@@ -39,51 +39,51 @@ describe('rpm', () => {
     expect(rpm(0, 0)).toBe(0);
   });
 
-  it('correctly multplies 2 and 5', () => {
+  it('correctly multiplies 2 and 5', () => {
     expect(rpm(2, 5)).toBe(10);
   });
 
-  it('correctly multplies -2 and 5', () => {
+  it('correctly multiplies -2 and 5', () => {
     expect(rpm(-2, 5)).toBe(-10);
   });
 
-  it('correctly multplies 2 and -5', () => {
+  it('correctly multiplies 2 and -5', () => {
     expect(rpm(2, -5)).toBe(-10);
   });
 
-  it('correctly multplies -2 and -5', () => {
+  it('correctly multiplies -2 and -5', () => {
     expect(rpm(-2, -5)).toBe(10);
   });
 
-  it('correctly multplies 5 and 2', () => {
+  it('correctly multiplies 5 and 2', () => {
     expect(rpm(5, 2)).toBe(10);
   });
 
-  it('correctly multplies -5 and 2', () => {
+  it('correctly multiplies -5 and 2', () => {
     expect(rpm(-5, 2)).toBe(-10);
   });
 
-  it('correctly multplies 5 and -2', () => {
+  it('correctly multiplies 5 and -2', () => {
     expect(rpm(5, -2)).toBe(-10);
   });
 
-  it('correctly multplies -5 and -2', () => {
+  it('correctly multiplies -5 and -2', () => {
     expect(rpm(-5, -2)).toBe(10);
   });
 
-  it('correctly multplies 100000 and 100000', () => {
+  it('correctly multiplies 100000 and 100000', () => {
     expect(rpm(100000, 100000)).toBe(10000000000);
   })
 
-  it('correctly multplies -100000 and 100000', () => {
+  it('correctly multiplies -100000 and 100000', () => {
     expect(rpm(-100000, 100000)).toBe(-10000000000);
   });
 
-  it('correctly multplies 100000 and -100000', () => {
+  it('correctly multiplies 100000 and -100000', () => {
     expect(rpm(100000, -100000)).toBe(-10000000000);
   });
 
-  it('correctly multplies -100000 and -100000', () => {
+  it('correctly multiplies -100000 and -100000', () => {
     expect(rpm(-100000, -100000)).toBe(10000000000);
   });
 
